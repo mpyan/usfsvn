27f2f8a68b5ad032626f48d6a85e2d56cadd3c34
mpyan <mpyan@a170c6c2-a73f-4b33-81ae-61c44713ea7b>
2014-09-10 06:09:04 +0000

    edit comments
    git-svn-id: https://www.cs.usfca.edu/svn/mpyan@9 a170c6c2-a73f-4b33-81ae-61c44713ea7b

--- cs220/p1/solve_heat_eqn.c
+++ cs220/p1/solve_heat_eqn.c
@@ -11,7 +11,7 @@
  *
  *           ./solve_heat_eqn < data
  *
- *			 (where "data" is the file containing the inputs)
+ *           (where "data" is the file containing the inputs)
  *
  * Input:    m, the number of grid points in the x-direction is m+1
  *           n, the number of grid points in the t-direction is n+1
