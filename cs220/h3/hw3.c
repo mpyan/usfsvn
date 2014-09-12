4e443e7bcb85d6fa2c66e8a6f4b15ed1986e0900
mpyan <mpyan@a170c6c2-a73f-4b33-81ae-61c44713ea7b>
2014-09-12 01:13:59 +0000

    use linked list to store locations
    git-svn-id: https://www.cs.usfca.edu/svn/mpyan@11 a170c6c2-a73f-4b33-81ae-61c44713ea7b

--- cs220/h3/hw3.c
+++ cs220/h3/hw3.c
@@ -24,12 +24,20 @@ const int MAX_STRING_LEN = 100;
 
 void Usage(char prog_name[]);
 
+struct node {
+   int val;
+   struct node *next_p;
+};    
+
 int main(int argc, char* argv[]) {
    char search_for[MAX_STRING_LEN];
    char current_word[MAX_STRING_LEN];
    int  scanf_rv;
    int  word_count = 0;  
    
+   struct node *root_found_p;
+   struct node *current_found_p;
+   struct node *head_found_p;
    int num_appearances = 0;
 
    /* Check that the user-specified word is on the command line */
@@ -38,24 +46,45 @@ int main(int argc, char* argv[]) {
 
    printf("Enter the text to be searched\n");
    scanf_rv = scanf("%s", current_word);
+   
+   root_found_p = 0;
+   current_found_p = 0;
+   head_found_p = 0;
+   
    while ( scanf_rv != EOF ){
       if (strcmp(current_word, search_for) == 0){
-          if (num_appearances == 0){
-                printf("\n%s was word number %d", search_for, word_count);     
-          } else {
-                printf(", %d", word_count);
-          }
-          num_appearances++;
+         current_found_p = (struct node *)malloc(sizeof(struct node));
+         current_found_p->val = word_count;
+         if (num_appearances == 0){
+            root_found_p = current_found_p;
+         } else {
+            head_found_p->next_p = current_found_p;
+         }
+         head_found_p = current_found_p;
+         
+         num_appearances++;
       }
-      word_count++; 
+      word_count++;
       scanf_rv = scanf("%s", current_word);
    }
+   
+   current_found_p = root_found_p;
 
-   if (num_appearances == 0)
+   if (num_appearances == 0){
       printf("\n%s was not found in the %d words of input\n",
          search_for, word_count);
-   else
+   }
+   else {
+      printf("%s was word number(s)", search_for);
+      int i;
+      for (i = 0; i < num_appearances; i++){
+         if (i != 0) printf(",");
+         if (i == (num_appearances - 1)) printf(" and");
+         printf(" %d", current_found_p->val);
+         current_found_p = current_found_p->next_p;
+      }
       printf("\n%s was found %d times.\n", search_for, num_appearances);
+   }
 
    return 0;
 }  /* main */
