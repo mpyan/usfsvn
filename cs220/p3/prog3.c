a88547d318cfe9b41f1c5e06770fd5605baa9169
mpyan <mpyan@a170c6c2-a73f-4b33-81ae-61c44713ea7b>
2014-10-13 02:20:35 +0000

    Prog3: add read/print matrix
    git-svn-id: https://www.cs.usfca.edu/svn/mpyan@39 a170c6c2-a73f-4b33-81ae-61c44713ea7b

--- cs220/p3/prog3.c
+++ cs220/p3/prog3.c
@@ -1,9 +1,16 @@
 /* prog3.c
+ * Compile:  mpicc -g -Wall -o prog3 prog3.c
+ * Run:      mpiexec -n <number of processes> ./prog3
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
+
+const int INFINITY = 1000000;
+
+void Read_matrix(int mat[], int n);
+void Print_matrix(int mat[], int n);
 
 int main(void) {
    int p, my_rank;
@@ -29,13 +36,50 @@ int main(void) {
    if (my_rank == 0) {
       printf("Enter n\n");
       scanf("%d", &n);
+      /* Read in the matrix */
+      Read_matrix(mat, n);
    }
    MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
    MPI_Scatter(temp_mat, n*n/p, MPI_INT, local_mat, n*n/p, MPI_INT, 0, comm);
 
    /* Print the matrix when done */
    MPI_Gather(local_mat, n*n/p, MPI_INT, temp_mat, n*n/p, MPI_INT, 0, comm);
+   if (my_rank == 0) {
+   	  Print_matrix(temp_mat, n);
+   }
 
    MPI_Finalize();
    return 0;
 }  /* main */
+
+/*-------------------------------------------------------------------
+ * Function:  Read_matrix
+ * Purpose:   Read in the adjacency matrix
+ * In arg:    n
+ * Out arg:   mat
+ */
+void Read_matrix(int mat[], int n) {
+   int i, j;
+
+   for (i = 0; i < n; i++)
+      for (j = 0; j < n; j++)
+         scanf("%d", &mat[i*n+j]);
+}  /* Read_matrix */
+
+/*-------------------------------------------------------------------
+ * Function:  Print_matrix
+ * Purpose:   Print the contents of the matrix
+ * In args:   mat, n
+ */
+void Print_matrix(int mat[], int n) {
+   int i, j;
+
+   for (i = 0; i < n; i++) {
+      for (j = 0; j < n; j++)
+         if (mat[i*n+j] == INFINITY)
+            printf("i ");
+         else
+            printf("%d ", mat[i*n+j]);
+      printf("\n");
+   }
+}  /* Print_matrix */
