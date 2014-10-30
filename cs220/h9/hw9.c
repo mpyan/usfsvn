cb1e6949bb85a13ad79db31d82ab8b71bb80662b
mpyan <mpyan@a170c6c2-a73f-4b33-81ae-61c44713ea7b>
2014-10-30 04:29:37 +0000

    hw9: functionality now works
    git-svn-id: https://www.cs.usfca.edu/svn/mpyan@66 a170c6c2-a73f-4b33-81ae-61c44713ea7b

--- cs220/h9/hw9.c
+++ cs220/h9/hw9.c
@@ -12,7 +12,88 @@ double* x = NULL;
 double* y = NULL;
 int n;
 
+/* Serial functions */
 void Usage(char* prog_name);
+void Print_vector(char* title, double y[], int m);
+void Read_vector(char* prompt, double x[], int n);
+
+/* Parallel function */
+void *Daxpy(void* rank);
+
+int main(int argc, char* argv[]){
+	long thread;
+	pthread_t* thread_handles = NULL;
+
+	/* Get number of threads from command line */
+   if (argc != 2) Usage(argv[0]);
+   thread_count = strtol(argv[1], NULL, 10);
+   if (thread_count <= 0) Usage(argv[0]);
+
+   thread_handles = malloc(thread_count*sizeof(pthread_t));
+
+   /* Read in n */
+   printf("Please enter n: ");
+   scanf("%d", &n);
+
+   /* Allocate and initialize */
+   x = malloc(n*sizeof(double));
+   y = malloc(n*sizeof(double));
+
+   /* Read in x, y and alpha */
+   Read_vector("Enter x", x, n);
+   Read_vector("Enter y", y, n);
+   printf("Please enter alpha: ");
+   scanf("%lf", &alpha);
+
+   for (thread = 0; thread < thread_count; thread++)
+      pthread_create(&thread_handles[thread], NULL, 
+         Daxpy, (void*) thread);
+
+   for (thread = 0; thread < thread_count; thread++)
+      pthread_join(thread_handles[thread], NULL);
+
+   /* Print the result */
+   Print_vector("Result: ", y, n);
+
+   free(x);
+   free(y);
+   free(thread_handles);
+} /* main */
+
+/*-------------------------------------------------------------------*/
+void Usage(char* prog_name) {
+   fprintf(stderr, "usage: %s <number of threads>\n", prog_name);
+   fprintf(stderr, "number of threads > 0\n");
+   exit(0);
+}  /* Usage */
+
+/*------------------------------------------------------------------
+ * Function:    Print_vector
+ * Purpose:     Print a vector
+ * In args:     title, y, m
+ */
+void Print_vector(char* title, double y[], int m) {
+   int   i;
+
+   printf("%s\n", title);
+   for (i = 0; i < m; i++)
+      printf("%4.1f ", y[i]);
+   printf("\n");
+}  /* Print_vector */
+
+/*------------------------------------------------------------------
+ * Function:        Read_vector
+ * Purpose:         Read in the vector x
+ * In arg:          prompt, n
+ * Out arg:         x
+ */
+void Read_vector(char* prompt, double x[], int n) {
+   int   i;
+
+   printf("%s\n", prompt);
+   for (i = 0; i < n; i++) 
+      scanf("%lf", &x[i]);
+}  /* Read_vector */
 
 void *Daxpy(void* rank){
    long my_rank = (long) rank;
@@ -27,61 +108,3 @@ void *Daxpy(void* rank){
 
    return NULL;
 } /* Daxpy */
-
-int main(int argc, char* argv[]){
-	long thread;
-	pthread_t* thread_handles = NULL;
-
-	/* Get number of threads from command line */
-   if (argc != 2) Usage(argv[0]);
-   thread_count = strtol(argv[1], NULL, 10);  
-   if (thread_count <= 0) Usage(argv[0]);
-
-   /* Read in n */
-   printf("Please enter n: ");
-   scanf("%d", &n);
-
-   /* Allocate and initialize */
-   x = malloc(n*sizeof(double));
-   y = malloc(n*sizeof(double));
-
-   /* Read in x, y and alpha */
-   // printf("Please enter alpha: ");
-   // scanf("%lf", &alpha);
-   // printf("got alpha");
-   alpha = 1.0;
-
-   printf("Please enter x:\n");
-   int i;
-   for (i = 0; i < n; i++){
-      scanf("%lf", &x[i]);
-   }
-   printf("Please enter y:\n");
-   for (i = 0; i < n; i++){
-      scanf("%lf", &y[i]);
-   }
-
-   printf("done");
-
-   for (thread = 0; thread < thread_count; thread++)
-      pthread_create(&thread_handles[thread], NULL, 
-         Daxpy, (void*) thread);
-
-   for (thread = 0; thread < thread_count; thread++)
-      pthread_join(thread_handles[thread], NULL);
-
-   /* Print the result */
-   printf("Result: \n");
-   for (i = 0; i < n; i++)
-      printf("%4.1f ", y[i]);
-
-   free(x);
-   free(y);
-} /* main */
-
-/*-------------------------------------------------------------------*/
-void Usage(char* prog_name) {
-   fprintf(stderr, "usage: %s <number of threads>\n", prog_name);
-   fprintf(stderr, "number of threads > 0\n");
-   exit(0);
-}  /* Usage */
