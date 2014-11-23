bfd7b896fa936294d3c911cacfb21bff29870308
mpyan <mpyan@a170c6c2-a73f-4b33-81ae-61c44713ea7b>
2014-11-23 19:18:50 +0000

    prog5: add Generate_list and Print_list functions
    git-svn-id: https://www.cs.usfca.edu/svn/mpyan@113 a170c6c2-a73f-4b33-81ae-61c44713ea7b

--- cs220/p5/pth_bitonic.c
+++ cs220/p5/pth_bitonic.c
@@ -11,16 +11,45 @@
 #include <pthread.h>
 #include "timer.h"
 
+const int RMAX = 999999;
+
 int thread_count;
+int n;
+int* good_list;
+int* temp_list;
 
 void Usage(char* prog_name);
+void Print_list(char* text, int list[]){
+	int i;
+	printf("%s", text);
+	for (i = 0; i < n; i++){
+		printf("%d ", list[i]);
+	}
+	printf("\n");
+}
+void Generate_list(){
+	srandom(1);
+	int i;
+	for (i = 0; i < n; i++){
+		good_list[i] = random() % RMAX;
+	}
+}
+void Get_list(){
+	int i;
+	for (i = 0; i < n; i++){
+		scanf("%d", &good_list[i]);
+	}
+}
 
 int main(int argc, char* argv[]) {
 	int has_g = 0;
 	int has_o = 0;
+	double start, finish;
 
 	/* Check input args */
 	if (argc < 3) Usage(argv[0]);
+	thread_count = strtol(argv[1], NULL, 10);
+   	n = strtol(argv[2], NULL, 10);
 	if (argc > 3){
 		if (strcmp(argv[3], "g") == 0){
 			has_g = 1;
@@ -34,6 +63,30 @@ int main(int argc, char* argv[]) {
 				has_o = 1;
 		}
 	}
+
+	good_list = malloc(n*sizeof(int));
+	temp_list = malloc(n*sizeof(int));
+
+	if (has_g == 1){
+		/* generate list */
+		Generate_list();
+	} else {
+		/* read in the list from stdin */
+		Get_list();
+	}
+	if (has_o == 1) /* maybe use memcpy here, move this code elsewhere? */
+		Print_list("original: ", good_list);
+
+	GET_TIME(start);
+	/* Create threads */
+	/* Join threads */
+	GET_TIME(finish);
+	printf("Elapsed time = %e seconds\n", finish-start);
+
+	/* Print the list */
+	Print_list("result: ", good_list);
+
+	/* Free */
 
 	return 0;
 }
