9ae5ca2bd6ce16bf0bf8e02b9e482b718eb29285
mpyan <mpyan@a170c6c2-a73f-4b33-81ae-61c44713ea7b>
2014-11-25 19:40:19 +0000

    prog5: add local qsort
    git-svn-id: https://www.cs.usfca.edu/svn/mpyan@118 a170c6c2-a73f-4b33-81ae-61c44713ea7b

--- cs220/p5/pth_bitonic.c
+++ cs220/p5/pth_bitonic.c
@@ -18,8 +18,13 @@ int n;
 int* good_list;
 int* temp_list;
 
+pthread_mutex_t barrier_mutex;
+pthread_cond_t cond_var;
+int barrier_counter = 0;
+
 void Usage(char* prog_name);
 void* Thread_work(void* rank);
+int  Compare(const void* a_p, const void* b_p);
 void Print_list(char* text, int list[]){
 	int i;
 	printf("%s", text);
@@ -90,6 +95,8 @@ int main(int argc, char* argv[]) {
 	if (has_o == 1) /* maybe use memcpy here, move this code elsewhere? */
 		Print_list("original: ", good_list);
 
+	pthread_mutex_init(&barrier_mutex, NULL);
+	pthread_cond_init(&cond_var, NULL);
 	/* Start work */
 	GET_TIME(start);
 	for (thread = 0; thread < thread_count; thread++)
@@ -123,6 +130,24 @@ void Usage(char* prog_name) {
    exit(0);
 }  /* Usage */
 
+/*-------------------------------------------------------------------
+ * Function:    Compare
+ * Purpose:     Compare 2 ints, return -1, 0, or 1, respectively, when
+ *              the first int is less than, equal, or greater than
+ *              the second.  Used by qsort.
+ */
+int Compare(const void* a_p, const void* b_p) {
+   int a = *((int*)a_p);
+   int b = *((int*)b_p);
+
+   if (a < b)
+      return -1;
+   else if (a == b)
+      return 0;
+   else /* a > b */
+      return 1;
+}  /* Compare */
+
 void* Thread_work(void* rank){
 	long my_rank = (long) rank;
 
@@ -130,23 +155,42 @@ void* Thread_work(void* rank){
 	unsigned and_bit = 2;
 	long partner;
 	int stage = 0;
+	int local_n = n/thread_count;
+	int my_head = my_rank * local_n;
+	int my_tail = my_head + local_n - 1;
+	int* swapper = NULL;
+
+	printf("Th %ld > start qsort, my_head = %d, my_tail = %d\n", my_rank, my_head, my_tail);
 
 	/* Quicksort this thread's own stuff */
+	qsort(good_list + my_head, local_n, sizeof(int), Compare);
 	/* Barrier here */
-
-	while (bitmask > 0){
-		partner = my_rank ^ bitmask;
-		if ((my_rank & and_bit) == 0)
-			Merge_inc(my_rank, partner, stage);
-		else
-			Merge_dec(my_rank, partner, stage);
-		bitmask >>= 1;
-		and_bit <<= 1;
-		stage++;
-		/* Barrier here? */
+	printf("Th %ld > local qsort done \n", my_rank);
+	pthread_mutex_lock(&barrier_mutex);
+	barrier_counter++;
+	if (barrier_counter == thread_count){
+		barrier_counter = 0;
+		pthread_cond_broadcast(&cond_var);
+	} else {
+		while (pthread_cond_wait(&cond_var, &barrier_mutex) != 0);
 	}
+	pthread_mutex_unlock(&barrier_mutex);
+	printf("Th %ld > exited barrier \n", my_rank);
+	/* End of Barrier */
 
-	/* Swap pointers of temp_list and good_list */
+	// while (bitmask > 0){
+	// 	partner = my_rank ^ bitmask;
+	// 	if ((my_rank & and_bit) == 0)
+	// 		Merge_inc(my_rank, partner, stage);
+	// 	else
+	// 		Merge_dec(my_rank, partner, stage);
+	// 	bitmask >>= 1;
+	// 	and_bit <<= 1;
+	// 	stage++;
+	// 	/* Barrier here? */
+	// }
+
+	/* Swap pointers of temp_list and good_list (in the barrier?) */
 
 	return NULL;
 }
